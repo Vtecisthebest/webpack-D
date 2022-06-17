aa4f06bb15d385e61b8ed6f350e9463b60deace6
dxt1996 <1245338539@qq.com>
2022-06-17 21:46:11 +0800

    字体设置成功

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,18 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|gif|jpeg)$/i,
-        type: 'asset'
-    }
+        type: 'asset',
+       generator:{
+        filename:'images/[hash:6][ext]'
+       }
+      } ,
+      { // webpack5默认内部不认识这些文件, 所以当做静态资源直接输出即可
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'font-[name].[hash:6][ext]'
+        }
+    }, 
     ]
   }
 }
